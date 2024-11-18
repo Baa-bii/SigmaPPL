d1b12df725c907632c34a2867c3a4b9004d4ecd0
Baa-bii <alba6259@gmail.com>
2024-11-18 21:20:15 +0700

    akademik view

--- resources/views/content/akademik/ruangan.blade.php
+++ resources/views/content/akademik/ruangan.blade.php
@@ -15,10 +15,71 @@
     <div class="antialiased bg-gray-50 dark:bg-gray-900">
         <x-header></x-header>
         <x-sidebar></x-sidebar>
-        <main>
-        
+        <main class="p-16 md:ml-64 h-auto pt-20">
+            <div class="font-sans font-semibold text-gray-600 m-5">
+                <h2>Kelola Ruangan</h2>
+            </div>
+
+            <div></div>
+            <div class="relative flex flex-col w-full h-full overflow-scroll text-gray-500 bg-white shadow-md rounded-xl bg-clip-border">
+                <table class="w-full text-left table-auto min-w-max">
+                    <thead>
+                    <tr>
+                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                        <p class="block font-sans text-lg antialiased font-semibold leading-none text-blue-700 ">
+                            Ruang
+                        </p>
+                        </th>
+                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50" data-sort="gedung">
+                        <p class="block font-sans text-lg antialiased font-semibold leading-none text-blue-700">
+                            Gedung
+                        </p>
+                        </th>
+                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50" data-sort="prodi">
+                        <p class="block font-sans text-lg antialiased font-semibold leading-none text-blue-700 ">
+                            Prodi
+                        </p>
+                        </th>
+                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                        <p class="block font-sans text-lg antialiased font-semibold leading-none text-blue-700 ">Action</p>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="p-4 border-b border-blue-gray-50">
+                        <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                            A101
+                        </p>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                        <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                            A
+                        </p>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                        <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                            Informatika
+                        </p>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                        <a href="#" class="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+                            <button class="bg-green-400 w-10 rounded text-white hover:bg-green-500 shadow-md">Edit</button>
+                        </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <a href="#">
+                    <button class="bg-blue-500 w-auto h-auto m-4 p-2 rounded text-white hover:bg-blue-600 shadow-lg">Tambahkan Ruangan</button>
+                </a>
+            </div>
         </main>
         <x-footerdosen></x-footerdosen>
     </div>
 </body>
+
+
 </html>
