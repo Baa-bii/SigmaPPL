65c89f61edbe3a750e8bdfca34650892cc7e8b04
keisya31 <keinala23@gmail.com>
2024-11-27 14:43:47 +0700

    isi seeder

---
+++ app/Models/IRS.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IRS extends Model
+{
+    use HasFactory;
+
+    protected $table = 'irs';
+
+    protected $fillable = ['nim', 'kode_mk', 'id_TA'];
+
+    /**
+     * Relasi ke tabel Mahasiswa.
+     */
+    public function mahasiswa()
+    {
+        return $this->belongsTo(Mahasiswa::class, 'nim', 'nim');
+    }
+
+    /**
+     * Relasi ke tabel Mata Kuliah.
+     */
+    public function matakuliah()
+    {
+        return $this->belongsTo(Matakuliah::class, 'kode_mk', 'kode_mk');
+    }
+
+    /**
+     * Relasi ke tabel Semester Aktif.
+     */
+    public function semesterAktif()
+    {
+        return $this->belongsTo(SemesterAktif::class, 'id_TA', 'id');
+    }
+}
--- database/seeders/IRSSeeder.php
+++ database/seeders/IRSSeeder.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\IRS;
 
 class IRSSeeder extends Seeder
 {
@@ -13,49 +13,250 @@ class IRSSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('irs')->insert([
-            [
-                'nim' => '24060122140999', // Pastikan NIM ini ada di tabel 'mahasiswa'
-                'kode_mk' => 'PAIK6102', // Pastikan kode ini ada di tabel 'matakuliah'
-                'id_TA' => 1, // Pastikan ID ini ada di tabel 'semester_aktif'
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'nim' => '24060122140999',
-                'kode_mk' => 'PAIK6702',
-                'id_TA' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'nim' => '24060122140999',
-                'kode_mk' => 'PAIK6406',
-                'id_TA' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'nim' => '24060122140999',
-                'kode_mk' => 'PAIK6404',
-                'id_TA' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'nim' => '24060122140999',
-                'kode_mk' => 'PAIK6301',
-                'id_TA' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'nim' => '24060122140999',
-                'kode_mk' => 'PAIK65204',
-                'id_TA' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+          // Ambil id_TA dari semester_aktif yang relevan
+          $id_TA_Ganjil_2022 = DB::table('semester_aktif')
+          ->where('tahun_akademik', '2022/2023 Ganjil')
+          ->where('semester', 1)
+          ->first()->id; // Ambil id semester aktif
+
+           // Ambil id_TA dari semester_aktif yang relevan
+           $id_TA_Genap_2022 = DB::table('semester_aktif')
+           ->where('tahun_akademik', '2022/2023 Genap')
+           ->where('semester', 2)
+           ->first()->id; // Ambil id semester aktif
+
+            // Ambil id_TA dari semester_aktif yang relevan
+          $id_TA_Ganjil_2023 = DB::table('semester_aktif')
+          ->where('tahun_akademik', '2023/2024 Ganjil')
+          ->where('semester', 3)
+          ->first()->id; // Ambil id semester aktif
+
+           // Ambil id_TA dari semester_aktif yang relevan
+           $id_TA_Genap_2023 = DB::table('semester_aktif')
+           ->where('tahun_akademik', '2023/2024 Genap')
+           ->where('semester', 4)
+           ->first()->id; // Ambil id semester aktif
+
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6102',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6101',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00007',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6105',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00005',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6103',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6104',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00003',
+            'id_TA' => $id_TA_Ganjil_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6202',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6204',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00011',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6603',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6203',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00004',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6402',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00006',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6201',
+            'id_TA' => $id_TA_Genap_2022,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6301',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6306',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6303',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6302',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6304',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6506',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6305',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'UUW00008',
+            'id_TA' => $id_TA_Ganjil_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6406',
+            'id_TA' => $id_TA_Genap_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6404',
+            'id_TA' => $id_TA_Genap_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6401',
+            'id_TA' => $id_TA_Genap_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6403',
+            'id_TA' => $id_TA_Genap_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6405',
+            'id_TA' => $id_TA_Genap_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        IRS::create([
+            'nim' => '24060122140999',
+            'kode_mk' => 'PAIK6601',
+            'id_TA' => $id_TA_Genap_2023,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
     }
 }
