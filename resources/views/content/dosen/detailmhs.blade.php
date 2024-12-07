2ab8b19112f8a1c8a0c5a2da084e76ecb85c9be9
byochiram <rosidahrahmati@gmail.com>
2024-12-07 12:37:31 +0700

    nambahin logo di title website di semua view

--- resources/views/content/dosen/detailmhs.blade.php
+++ resources/views/content/dosen/detailmhs.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Perwalian</title>
+    <title>Detail Mahasiswa</title>
+    <link rel="icon" href="{{ asset('img/fix.png') }}" type="image/png">
     @vite(['resources/css/app.css','resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
